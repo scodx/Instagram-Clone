e1ab614af8712e25736ac57b07203d177b2ae967
Oscar Sánchez <oscar.exe@gmail.com>
2015-07-04 19:09:04 -0400

    Getting and updating an item from parse

--- 01 - Hello world/ParseStarterProject/ViewController.swift
+++ 01 - Hello world/ParseStarterProject/ViewController.swift
@@ -13,15 +13,59 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        /*
         var product = PFObject(className: "Products")
         
-        product["name"] = "Pizza!"
-        product["description"] = "delicius!"
-        product["price"] = 8.99
+        product["name"] = "Enchiladas!"
+        product["description"] = "Echiladas!!!!!!!!!!!"
+        product["price"] = 7.99
         
         product.saveInBackgroundWithBlock{(success, error) -> Void in
-            println("saved to Parse!!")
+            println("saved to Parse with ID: \(product.objectId)")
         }
+        */
+        
+        // enchiladas ID: bkvKUZRMaF
+        
+        
+        var query = PFQuery(className: "Products")
+        
+        // Query an item by it´s ID and EDIT the item
+        query.getObjectInBackgroundWithId("bkvKUZRMaF", block: {(object: PFObject?, error: NSError? ) -> Void in
+            
+            if error != nil{
+                println(error)
+            }else if let product = object{
+                
+                product["description"] = "Enchiladas hechas en casa!!"
+                product["price"] = 9.00
+                
+                product.saveInBackground()
+                
+            }
+            
+        })
+        
+        
+        
+        // Query an item by it´s ID
+        
+        /*
+        query.getObjectInBackgroundWithId("bkvKUZRMaF", block: {(object, error ) -> Void in
+        
+            if error != nil{
+                println(error)
+            }else{
+                let oName = object!.objectForKey("name")
+                let oDesc = object!.objectForKey("description")
+                println("Name: \(oName)), Description: \(oDesc)");
+            }
+        
+        })
+        */
+        
+        
+        
         
         
     }
